e4bd1c588411390a068d778da0b463da80343211
dulcefaz <darellanofaz02@gmail.com>
2025-05-30 12:42:06 -0600

    Add files via upload

---
+++ calculadora/main3.py
@@ -0,0 +1,14 @@
+import operaciones.suma as s 
+x=int (input("Ingresa un valor "))
+y=int(input("Ingresa otro valor"))
+print(s.sumar(x,y))
+
+import operaciones.resta  as s 
+x=int (input("Ingresa un valor "))
+y=int(input("Ingresa otro valor"))
+print(s.restar(x,y))
+
+import operaciones.multiplicacion  as s 
+x=int (input("Ingresa un valor "))
+y=int(input("Ingresa otro valor"))
+print(s.multiplicar(x,y))
