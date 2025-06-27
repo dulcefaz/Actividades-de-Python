1e0786596c8406aad83e80d7eafe97e6328b39b1
dulcefaz <darellanofaz02@gmail.com>
2025-06-27 11:46:56 -0600

    Add files via upload

---
+++ actividad_pywhatkit.py
@@ -0,0 +1,11 @@
+import pywhatkit 
+
+pywhatkit.search("que es la programacion orientada a objetos")
+
+pywhatkit.playonyt("python programacion para principiantes")
+
+pywhatkit.text_to_handwriting("hola, esto es una prueba con pywhatkit.", rgb= [0,0,200])
+
+print("acciones completadas")
+
+
